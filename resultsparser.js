131261d8d25168fe72a75309aa01513c45944c69
dchotzen-hartzell <dchotzen-hartzell19@punahou.edu>
2016-11-26 21:14:18 -1000

    Add rankings

--- resultsparser.js
+++ resultsparser.js
@@ -1,6 +1,7 @@
 var results = "5159|Cantina Band|Punahou|Honolulu|HI|USA|false|false|15|0|15|15|0|45|45|2|15|25|2|20|30|0|10|10|0|30|30|2|55|100|0|90|90|2|40|65|2|50|65|2|20|90|2|35|75|0|90|90|0|5|45|2|85|105>5902|Team Musubi|Punahou|Honolulu|HI|USA|false|false|15|0|20|20|0|10|10|0|20|20|0|10|10|2|55|90|2|10|45|0|10|10|0|10|10|0|40|40|2|10|50|2|0|85|0|15|15|2|90|120|0|35|35|2|25|30>6109| Punabots| Independent| Honolulu| HI| USA|false|false|15|2|10|55|0|90|90|2|10|61|2|90|130|0|55|55|2|30|275|0|55|55|2|45|55|2|25|110|0|20|20|2|30|50|2|0|120|0|60|60|0|50|50|2|45|90>6175|Space Cacti|Punahou|Honolulu|HI|USA|false|false|15|2|0|15|0|0|10|2|15|25|0|45|45|2|20|35|0|30|30|2|10|30|2|10|61|0|90|90|2|40|50|2|20|75|0|35|35|0|30|30|0|35|35|0|65|65>6176|BuffnBlue3|Punahou|Honolulu|HI|USA|false|false|15|0|30|30|2|90|130|2|20|155|2|90|130|2|55|95|2|10|40|2|15|40|2|10|40|0|35|35|0|85|85|0|30|30|0|40|40|0|60|60|2|20|45|0|35|35>6962|BnL Robotics|Punahou|Honolulu|HI|USA|false|false|15|0|15|15|2|45|60|2|5|155|0|1|1|0|20|20|2|30|70|2|20|85|0|10|10|0|10|10|2|40|50|0|20|20|0|65|65|0|20|20|0|10|10|0|35|35>6963|BuffnBlue5|Punahou|Honolulu|HI|USA|false|false|15|0|10|10|0|20|20|0|10|10|2|20|155|0|55|55|0|60|60|0|70|70|0|45|45|0|10|10|2|20|85|2|40|180|2|15|40|2|20|50|2|5|60|2|65|85>7160|Blazers|Mililani Middle|Mililani|HI|USA|false|false|10|0|20|20|0|15|15|0|10|10|0|5|45|0|25|25|0|55|55|0|40|40|2|1|45|0|20|20|2|45|90>7438|Hot Spot Robotics|Mililani High|Mililani|HI|USA|false|false|15|2|20|27|2|0|32|0|39|39|2|1|52|2|35|51|2|20|75|0|15|15|2|10|45|2|10|85|0|70|70|2|65|80|2|50|90|2|60|85|2|20|45|2|50|80>7548|Velocity|Punahou|Honolulu|HI|USA|false|false|15|2|15|40|0|0|10|0|15|15|0|20|20|0|10|10|2|30|40|0|10|10|1|30|30|2|10|50|0|20|20|2|55|70|2|0|120|0|40|40|0|60|60|0|50|50>7549|Matrix|Punahou|Honolulu|HI|USA|false|false|15|0|30|30|2|10|70|0|90|90|2|20|30|0|20|20|0|40|40|2|20|90|2|30|120|2|10|85|0|25|25|2|10|65|2|40|70|2|40|130|2|50|110|0|65|65>7550|Fig Newtons|Punahou|Honolulu|HI|USA|false|false|15|2|30|70|0|90|90|2|10|80|2|55|95|2|55|90|2|60|140|2|10|70|2|90|140|2|25|110|2|50|65|2|40|180|2|65|145|2|90|120|2|70|120|2|85|105>8740|Iobotics|Iolani|Honolulu|HI|USA|false|false|15|2|20|27|0|45|45|2|39|50|0|20|20|0|20|20|0|30|30|2|70|90|1|30|30|0|25|95|2|20|85|2|25|45|2|40|70|2|60|105|2|40|50|2|10|115>8741|Iolani|Iolani|Honolulu|HI|USA|false|false|15|2|15|40|2|0|32|2|39|50|0|45|45|0|35|35|0|10|10|0|55|55|0|25|95|0|20|20|2|10|50|2|55|70|0|50|50|0|60|60|2|60|105|2|65|230>8795|TrojanBots|St Anthony Jr/Sr High|Wailuku|HI|USA|false|true|10|0|25|25|0|50|50|0|1|1|0|50|50|2|35|90|0|25|25|1|30|30|0|11|11|2|10|36|0|25|35>8898|Iolani|Iolani|Honolulu|HI|USA|false|false|15|0|20|20|2|5|20|2|5|155|2|1|52|2|10|60|2|10|45|2|70|90|2|90|140|2|40|65|2|20|65|0|65|65|0|40|40|2|60|90|2|50|110|0|10|10>8925|SASieBots|St Anthony Jr/Sr High|Wailuku|HI|USA|false|true|10|0|10|10|2|40|105|0|1|1|0|40|40|2|50|80|2|25|30|1|30|30|0|11|11|0|10|10|0|25|35>9066|Crouton Free|Punahou|Honolulu|HI|USA|false|false|15|2|10|55|0|10|10|0|20|20|2|10|80|0|55|55|2|30|275|2|60|140|1|30|30|0|40|40|0|10|10|0|10|10|2|65|145|2|10|50|0|10|10|0|65|65>9378|Falcons|Kalani High|Honolulu|HI|USA|false|false|15|2|0|15|2|45|60|0|5|5|2|45|65|0|35|35|0|40|40|0|15|15|2|10|50|2|35|40|2|35|70|0|55|55|2|10|65|2|45|80|2|10|115|2|35|90>9772|LJA Bulldogs|Le Jardin|Kailua|HI|USA|false|false|15|0|20|20|0|5|5|0|15|15|2|45|65|2|20|35|2|30|40|0|60|60|0|20|20|2|20|65|0|10|10|0|0|0|0|65|65|0|10|10|2|5|60|2|35|70>9850|Hoco|Home|Ewa Beach|HI|USA|false|false|15|0|10|10|2|10|30|0|20|20|0|40|40|0|55|55|0|10|10|2|55|100|1|30|30|2|20|30|0|50|50|0|20|20|2|15|55|2|10|45|2|10|40|2|65|85>10374|Bulldogs|Kaimuki High|Honolulu|HI|USA|false|false|10|0|0|0|2|15|55|0|20|20|0|10|10|0|85|85|0|25|25|0|0|0|2|1|45|0|45|45|0|65|65>10512|Eagles|Washington Middle|Honolulu|HI|USA|false|true|15|0|10|10|0|35|35|0|10|10|0|90|90|2|25|30|2|30|50|0|40|40|0|60|60|0|45|45|0|50|50|2|25|30|1|30|30|2|11|40|2|10|36|2|25|80>11165|O-Bots|Punahou|Honolulu|HI|USA|false|false|15|2|20|85|2|90|130|2|20|95|0|90|90|0|40|40|0|30|30|0|70|70|2|10|50|0|20|20|0|50|50|0|65|65|2|40|105|2|40|130|2|45|80|0|45|45>11845|Tiki Techs|Kealakehe High|Kailua Kona|HI|USA|false|true|10|2|65|80|0|0|0|0|40|40|2|65|230|0|35|35|0|25|25|1|30|30|2|11|40|0|10|10|2|25|80>11858|bluefish B|Windward Nazarene|Kaneohe|HI|USA|false|false|15|0|0|0|2|5|20|0|5|5|2|20|30|2|35|51|2|0|85|2|35|75|0|10|10|0|10|10|2|70|120|0|30|30|2|50|90|0|40|40|2|40|50|0|10|10>11859|bluefish A|Windward Nazarene|Kaneohe|HI|USA|false|false|14|0|0|0|0|5|5|0|39|39|2|10|60|2|10|40|0|20|20|2|20|50|2|35|40|0|70|70|2|25|45|0|10|10|2|60|90|0|20|20|0|45|45";
 
 var teams = [];
+var sortedteams = [];
 var matchcount = 0;
 
 var init = false;
@@ -24,6 +25,24 @@ function Team(number, name, school, matchdata) {
     }
   })
   matchcount = Math.max(this.matches.length, matchcount);
+
+  this.qp = function() {
+    var best10 = this.sortedmatches.slice(0, 10)
+    var qp = 0;
+    for (var i = 0; i < best10.length; i++) {
+      qp += parseInt(best10[i].qp);
+    }
+    return qp;
+  }
+
+  this.rp = function() {
+    var best10 = this.sortedmatches.slice(0, 10)
+    var rp = 0;
+    for (var i = 0; i < best10.length; i++) {
+      rp += parseInt(best10[i].rp);
+    }
+    return rp;
+  }
 }
 
 function Match(qp, score1, score2) {
@@ -46,7 +65,15 @@ function parse() {
     var teaminfo = teamresults[i].split("|");
     teams.push(new Team(teaminfo[0], teaminfo[1], teaminfo[2], teaminfo.slice(9)));
   }
-  console.log(teams);
+
+  sortedteams = teams;
+  sortedteams.sort(function(a, b) {
+    if (!(a.qp() === b.qp())) {
+      return b.qp()-a.qp();
+    } else {
+      return b.rp()-a.rp();
+    }
+  })
 }
 
 function showResults(sorted) {
@@ -91,6 +118,7 @@ function showResults(sorted) {
     }
     table.appendChild(row);
   }
+  showRankings();
 }
 
 function clearTable() {
@@ -99,13 +127,40 @@ function clearTable() {
     return !(element.id === "header")
   })
   for (var i = 0; i < list.length; i++) {
-    document.getElementById("results-table").removeChild(list[i]);
+    try {
+      document.getElementById("results-table").removeChild(list[i]);
+    } catch(exception){}
+  }
+}
+
+function showRankings() {
+  for (var i = 0; i < sortedteams.length; i++) {
+    var row = document.createElement("tr");
+    var rank = document.createElement("td"), num = document.createElement("td"), name = document.createElement("td"), school = document.createElement("td");
+    rank.innerHTML = i + 1;
+    num.innerHTML = sortedteams[i].number;
+    name.innerHTML = sortedteams[i].name;
+    school.innerHTML = sortedteams[i].school;
+    row.appendChild(rank);
+    row.appendChild(num);
+    row.appendChild(name);
+    row.appendChild(school);
+
+    var qp = document.createElement("td");
+    var rp = document.createElement("td");
+    qp.innerHTML = sortedteams[i].qp();
+    rp.innerHTML = sortedteams[i].rp();
+    row.appendChild(qp);
+    row.appendChild(rp);
+    document.getElementById("rankings").appendChild(row);
   }
 }
 
 
 parse();
 showResults(false);
+
+
 
 document.getElementById("dontsort").addEventListener('click', function() {
   clearTable();
